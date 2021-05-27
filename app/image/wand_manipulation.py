ea07fbd3f332ec7d8633f00bf06ddb00b1afc24c
legitimatetheory5476 <75882191+legitimatetheory5476@users.noreply.github.com>
2021-05-27 20:34:40 +0530

    add cube (#42)
    * add cube method
    
    * Update wand_manipulation.py
    
    * Update image_routes.py
    
    * Update image_routes.py

--- app/image/wand_manipulation.py
+++ app/image/wand_manipulation.py
@@ -1,5 +1,6 @@
 from app.image.WandManip import wand
 from app.image.decorators import executor
+from wand.image import Image
 
 __all__ = (
     "charcoal",
@@ -14,7 +15,9 @@ __all__ = (
     #    "solar",
     "rainbow",
     "magik",
+    "cube",
 )
+
 
 
 @executor
@@ -132,3 +135,40 @@ def magik(image, scale: int = None):
     )
 
     return image
+
+
+@executor
+@wand
+def cube(image):
+    
+    def p(x):
+        return int(x / 3)
+    
+    image.resize(p(1000), p(860))
+    image.format = "png"
+    image.alpha_channel = "opaque"
+    
+    image1 = image
+    image2 = image1.clone()
+    
+    final = Image(width=p(2550), height=p(760) * 3)
+    final.format = "png"
+    
+    image1.shear(background="none", x=(-30))
+    image1.rotate(-30)
+    final.composite(image1, left=p(250), top=p(-230) + p(118))
+    image1.close()
+    
+    image2.shear(background="rgba(0,0,0,0)", x=30)
+    image2.rotate(-30)
+    image3 = image2.clone()
+    final.composite(image2, left=p(750) - p(72), top=p(630))
+    image2.close()
+    
+    image3.flop()
+    final.composite(image3, left=p(-250) + p(68), top=p(630))
+    image3.close()
+    
+    final.crop(left=80, top=40, right=665, bottom=710)
+    
+    return final
